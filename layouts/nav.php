5c977f5457f1b28193b3d6ce307c6d3ad403f69e
Guillaume <gdv.delvecchio75@gmail.com>
2020-03-18 16:09:00 +0100

    head and nav hmtl done

---
+++ layouts/nav.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name ="author" content= "Guillaume Del Vecchio">
+        <meta name ="description" content = "Photographer ">
+        <title> Solitude et Reflets </title>
+
+    </head>
+    <header>
+        <h1>Solitude et Reflets</h1>
+        <nav>
+            <ul>
+                <li><a href="home.php" title="Accueil" alt="Aller à l'Acceuil">Accueil</a></li>
+                <li><a href="gallery.php" title="Galerie" alt="Aller à la galerie">Galerie</a></li>
+                <li><a href="reservation.php" title="Reservation" alt="Aller Réserver">Reservation</a></li>
+                <li><a href="contact.php" title="Contacte" alt="nous contacter">Contacte</a></li>
+            </ul>
+    </header>
