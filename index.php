b62cd70f0b80326cd576843ea3f80217a2cb7d25
Guillaume <gdv.delvecchio75@gmail.com>
2020-03-23 11:41:22 +0100

    add index page caroussel, bio

--- index.php
+++ index.php
@@ -3,5 +3,29 @@
         include('layouts/nav.php'); 
 ?>
         <main>
+                <section class="carousel">
+			<figure>
+				<a href="https://placeholder.com/"><img src="https://via.placeholder.com/200" alt="#"></a>
+				<a href="https://placeholder.com/"><img src="https://via.placeholder.com/200" alt="#"></a>
+				<a href="https://placeholder.com/"><img src="https://via.placeholder.com/200" alt="#"></a>
+				<a href="https://placeholder.com/"><img src="https://via.placeholder.com/200" alt="#"></a>
+				<a href="https://placeholder.com/"><img src="https://via.placeholder.com/200" alt="#"></a>
+			</figure>
+                </section>
+                <hr>
+                <section class="bio">
+                        <h2>Biographie</h2>
+                        <div>
+                                <p class="imgAuthor"><a href="https://placeholder.com/"><img src="https://via.placeholder.com/200" alt="#"></a></p>
+                                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id lorem vitae massa pulvinar sodales sollicitudin vitae turpis. Sed felis urna, mollis quis nisi eu, viverra rutrum nisl. Sed auctor a eros vel fermentum. In facilisis dapibus odio. Duis sodales ligula euismod sagittis condimentum. Mauris ac dui id quam tristique varius vel a ante. Proin ullamcorper urna a varius tempus. Morbi sodales efficitur risus, eget iaculis lectus auctor ac. Praesent gravida sapien vitae sapien fringilla, vel venenatis erat posuere. Vestibulum imperdiet convallis cursus. Fusce vulputate rhoncus orci ornare mollis. Suspendisse ullamcorper, magna nec aliquam blandit, urna justo fermentum nulla, ut tristique lacus nibh vel nulla. Etiam luctus ut risus quis elementum. Sed convallis ligula est, eget imperdiet elit rutrum non.
+
+Phasellus condimentum purus et ex rhoncus fringilla. Aliquam hendrerit consequat rutrum. Suspendisse in ullamcorper urna, et faucibus dui. Nunc suscipit ex a vulputate condimentum. Vivamus a lacus non diam commodo eleifend a sed sapien. Donec consequat scelerisque risus quis tincidunt. Curabitur aliquet venenatis mi, nec accumsan urna tempor vitae. Cras ac diam nec est condimentum imperdiet in a justo. Duis sagittis interdum turpis vitae ultrices.  Morbi in diam in eros fringilla euismod eget sed libero. Etiam nec tortor nec mauris accumsan dapibus dapibus a metus. In hac habitasse platea dictumst. Sed vel feugiat ante. Integer ornare volutpat odio at mollis. Donec ac massa non est facilisis dictum. Vivamus sed sapien sit amet felis bibendum interdum non at nisl. Sed sit amet gravida metus. Fusce pellentesque, lorem et efficitur dapibus, felis sapien viverra dolor, faucibus rutrum lorem purus nec nunc. Nullam eu consectetur lacus, nec tincidunt ipsum. Aliquam fringilla sagittis nibh, quis mollis nisl congue sed. In hac habitasse platea dictumst. Phasellus in laoreet est, eget condimentum lorem.
+
+Ut feugiat sem id enim fermentum, at imperdiet risus congue. Donec ornare lobortis orci, vel pretium lacus interdum quis. Aliquam risus sem, dictum nec orci quis, posuere tincidunt odio. Morbi euismod, ante posuere ornare eleifend, nulla purus lacinia nulla, a tempor purus enim volutpat magna. Quisque sit amet egestas ante, eu volutpat ligula. Maecenas euismod justo quis facilisis lacinia. Nam ac nisi vel leo faucibus ultrices. In finibus aliquam odio, sagittis elementum est vestibulum at. Quisque aliquam tortor non molestie luctus. Aenean pharetra enim eget rutrum molestie. Maecenas pretium pulvinar mauris eget vehicula. Suspendisse potenti. Ut luctus viverra magna. Mauris posuere justo felis, ut tristique risus volutpat quis. Quisque in rutrum turpis. </p>
+                        </div>
+                        <div class="seeMore">
+                                <a href="pages/gallery.php">Photographies</a>
+                        </div>
+                </section>
         </main>
 <?php include('layouts/footer.php'); ?>
